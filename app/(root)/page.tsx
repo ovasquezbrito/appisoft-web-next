9ad5f3295fc6e8e5633b7019207682ad65415889
ovasquezbrito <ovasquezcyt@gmail.com>
2024-11-01 13:47:08 -0400

    add shadcn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
+import SearchForm from "@/components/SearchForm";
 
 
-export default function Home() {
+export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
+
+  const query = (await searchParams).query;
   return (
     <>
-      <h1 className="text-3xl font-bold">HOME</h1>
+    <section className="pink_container">
+
+      <h1 className="heading">Presenta tu startup, <br /> y conéctate con emprendedores</h1>
+      <p className="sub-heading !max-w-3xl">Envíe ideas, vote propuestas y hágase notar en concursos virtuales.</p>
+      <SearchForm query={query}/>
+    </section>
     </>
   );
 }
